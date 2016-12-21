d1867bd3f89150f32201c593a37a1dd8d90ee7fe
saharudra <rudra.saha@asu.edu>
2016-12-21 12:00:07 -0700

    Update appRudra.js

--- appRudra.js
+++ appRudra.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    angular.module('appNakul', ['ngRoute' ])
+    angular.module('appRudra', ['ngRoute' ])
         .config(function ($routeProvider) {
             $routeProvider.when('/home', {
                 title: 'Home',
