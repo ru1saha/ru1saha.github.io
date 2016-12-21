b9ca3cc00aff23c91e42628284a7ebdde8b81f2c
saharudra <rudra.saha@asu.edu>
2016-12-21 12:13:53 -0700

    Update indexCtrl.js

--- shared/indexCtrl.js
+++ shared/indexCtrl.js
@@ -1,6 +1,6 @@
 (function(){
    'use strict';
-    angular.module('appNakul').controller('indexCtrl', function ($rootScope){
+    angular.module('appRudra').controller('indexCtrl', function ($rootScope){
 
         $rootScope.spinning = false;
     });
