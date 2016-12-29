bfa236eca124b0e9d02b9897ea6b7ec7f3f14657
Nakul Chawla <nakulchawla@Nakuls-MacBook-Pro.local>
2016-12-28 17:44:06 -0700

    fullpage scroll

--- home/homeCtrl.js
+++ home/homeCtrl.js
@@ -2,6 +2,15 @@
     'use strict';
     angular.module('appRudra').controller('homeCtrl',['$scope','$rootScope','$routeParams','$route', '$location', function($scope,$rootScope,$routeParams,$route,$location){
 
+        init();
+
+        function init(){
+
+            $('#fullpage').fullpage();
+
+        //    https://github.com/alvarotrigo/fullPage.js
+        }
+
 
     }]);
 
--- shared/common.js
+++ shared/common.js
@@ -2,6 +2,6 @@
  * Created by rudra saha on 12/21/2016.
  */
 (function(){
-    var appNakul = angular.module('appRudra',[]);
+    var appRudra = angular.module('appRudra',[]);
 
 })();
